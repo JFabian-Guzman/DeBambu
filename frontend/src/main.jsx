53df745f6a7ed2ef2ac6fdb29b06583d42a5452e
JFabian-Guzman <fabiguzmang1205@gmail.com>
2025-01-26 10:54:34 -0600

    Home page design v.1

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Auth from './modules/Auth/screens/Auth.jsx'
+import HomeScreen from './modules/Home/screens/Home.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Auth />
+    <HomeScreen />
   </StrictMode>,
 )
