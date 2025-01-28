a2577c5f1b351f0c473f66628d8e38b2386e74fb
JFabian-Guzman <fabiguzmang1205@gmail.com>
2025-01-28 17:57:10 -0600

    NavBar

--- frontend/src/modules/Shared/components/Navbar.jsx
+++ frontend/src/modules/Shared/components/Navbar.jsx
@@ -1,42 +1,56 @@
 
-"use client";
+import { Button, Navbar } from "flowbite-react";
 
-import { Navbar } from "flowbite-react";
+import logo from "../../../assets/logo_bambu.jpeg";
 
 const theme = {
+  "root": {
+    "base": "bg-transparent fixed z-10 w-full top-0 my-0 py-5",
+    "rounded": {
+      "on": "rounded",
+      "off": ""
+    },
+  },
   "brand": {
     "base": "flex items-center text-white hover:text-white",
   },
   "link": {
-    "base": "block py-2 pl-3 pr-4 md:p-0 text-white hover:text-green-500 text-lg",
+    "base": "flex items-center justify-center py-2 px-3 text-lg",
     "active": {
-      "on": "bg-green-50 text-white dark:text-white md:bg-transparent md:text-green-700",
-      "off": "text-white border-b border-gray-100 hover:bg-gray-50 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:hover:bg-transparent md:hover:text-green-700 md:dark:hover:bg-transparent md:dark:hover:text-white"
+      "on": "text-green-600 bg-transparent hover:text-green-700",
+      "off": "text-white bg-transparent hover:text-green-700 "
     },
     "disabled": {
       "on": "text-gray-400 hover:cursor-not-allowed dark:text-gray-600",
       "off": ""
     }
-  },
+  }
 };
+
 
 const Nav = () => {
   return (
-    <Navbar fluid rounded className="fixed z-10 w-full top-0 bg-transparent my-0 py-3 sm:px-12 md:px-24 lg:px-32 xl:px-40 2xl:px-48" theme={theme}>
-      <Navbar.Brand href="/">
-      <span className="self-center whitespace-nowrap text-xl font-semibold ">DeBambu</span>
-      </Navbar.Brand>
-      <Navbar.Toggle />
-      <Navbar.Collapse>
-      <Navbar.Link href="#" active >
-      Home
-      </Navbar.Link>
-      <Navbar.Link href="#"  >Services</Navbar.Link>
-      <Navbar.Link href="#"  >Pricing</Navbar.Link>
-      <Navbar.Link href="#"  >Contact</Navbar.Link>
-      </Navbar.Collapse>
-    </Navbar>
-    );
+    <div className="flex items-center h-screen">
+      <Navbar rounded className="w-full" theme={theme}>
+        <Navbar.Brand href="/">
+          <span className="self-center whitespace-nowrap text-xl font-semibold">DeBambu</span>
+        </Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse className="flex justify-center items-center">
+          <Navbar.Link href="#" active>
+            Inicio
+          </Navbar.Link>
+          <Navbar.Link href="#" className="flex justify-center items-center">
+            Tienda
+          </Navbar.Link>
+          <Button className="bg-green-700">
+              Iniciar Sesión
+          </Button>
+          <Navbar.Toggle />
+        </Navbar.Collapse>
+      </Navbar>
+    </div>
+  );
 }
 
 export default Nav;
